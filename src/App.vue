cfafcaecf710bc220755d26004dda362c1729481
Oleg <deikun95@gmail.com>
2020-05-13 00:11:56 +0300

    Started to make design of the main page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,283 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="wrapper">
+    <div class="header">
+      <div class="container">
+        <div class="header__content">
+          <div class="logo header__logo">
+            <img src="./assets/img/logo-restore.svg" alt />
+          </div>
+          <div class="navbar header__navbar">
+            <ul class="navbar__list">
+              <li class="navbar__item">
+                <a class="navbar__link" href>Главная</a>
+              </li>
+              <li class="navbar__item">
+                <a class="navbar__link" href>Каталог</a>
+              </li>
+              <li class="navbar__item">
+                <a class="navbar__link" href>Контакты</a>
+              </li>
+              <li class="navbar__item">
+                <a class="navbar__link" href>Личный кабинет</a>
+              </li>
+            </ul>
+          </div>
+          <div class="cart navbar__cart">
+            <a class="navbar__link" href>
+              <i>
+                <svg
+                  class="icon navbar__icon"
+                  height="456pt"
+                  viewBox="0 -13 456.75885 456"
+                  width="456pt"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="m150.355469 322.332031c-30.046875 0-54.402344 24.355469-54.402344 54.402344 0 30.042969 24.355469 54.398437 54.402344 54.398437 30.042969 0 54.398437-24.355468 54.398437-54.398437-.03125-30.03125-24.367187-54.371094-54.398437-54.402344zm0 88.800781c-19 0-34.402344-15.402343-34.402344-34.398437 0-19 15.402344-34.402344 34.402344-34.402344 18.996093 0 34.398437 15.402344 34.398437 34.402344 0 18.996094-15.402344 34.398437-34.398437 34.398437zm0 0"
+                  />
+                  <path
+                    d="m446.855469 94.035156h-353.101563l-7.199218-40.300781c-4.4375-24.808594-23.882813-44.214844-48.699219-48.601563l-26.101563-4.597656c-5.441406-.96875-10.632812 2.660156-11.601562 8.097656-.964844 5.441407 2.660156 10.632813 8.101562 11.601563l26.199219 4.597656c16.53125 2.929688 29.472656 15.871094 32.402344 32.402344l35.398437 199.699219c4.179688 23.894531 24.941406 41.324218 49.199219 41.300781h210c22.0625.066406 41.546875-14.375 47.902344-35.5l47-155.800781c.871093-3.039063.320312-6.3125-1.5-8.898438-1.902344-2.503906-4.859375-3.980468-8-4zm-56.601563 162.796875c-3.773437 12.6875-15.464844 21.367188-28.699218 21.300781h-210c-14.566407.039063-27.035157-10.441406-29.5-24.800781l-24.699219-139.398437h336.097656zm0 0"
+                  />
+                  <path
+                    d="m360.355469 322.332031c-30.046875 0-54.402344 24.355469-54.402344 54.402344 0 30.042969 24.355469 54.398437 54.402344 54.398437 30.042969 0 54.398437-24.355468 54.398437-54.398437-.03125-30.03125-24.367187-54.371094-54.398437-54.402344zm0 88.800781c-19 0-34.402344-15.402343-34.402344-34.398437 0-19 15.402344-34.402344 34.402344-34.402344 18.996093 0 34.398437 15.402344 34.398437 34.402344 0 18.996094-15.402344 34.398437-34.398437 34.398437zm0 0"
+                  />
+                </svg>
+              </i>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <div class="promo">
+      <div class="promo__img"></div>
+    </div>
+    <div class="content">
+      <div class="container">
+        <div class="main-title content__main-title">
+          <p>Популярные товары</p>
+        </div>
+        <div class="trend content__trend">
+          <div class="card trend__card">
+            <div class="card__img"></div>
+            <div class="card__description">
+              <div class="card__text">
+                <span class="title card__item-title">iPhone 11 Pro</span>
+                <span class="price card__item-price">
+                  от
+                  <b>69999 руб.</b>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="card trend__card">
+            <div class="card__img"></div>
+            <div class="card__description">
+              <div class="card__text">
+                <span class="title card__item-title">iPhone 11 Pro</span>
+                <span class="price card__item-price">
+                  от
+                  <b>69999 руб.</b>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="card trend__card">
+            <div class="card__img"></div>
+            <div class="card__description">
+              <div class="card__text">
+                <span class="title card__item-title">iPhone 11 Pro</span>
+                <span class="price card__item-price">
+                  от
+                  <b>69999 руб.</b>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="card trend__card">
+            <div class="card__img"></div>
+            <div class="card__description">
+              <div class="card__text">
+                <span class="title card__item-title">iPhone 11 Pro</span>
+                <span class="price card__item-price">
+                  от
+                  <b>69999 руб.</b>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="card trend__card">
+            <div class="card__img"></div>
+            <div class="card__description">
+              <div class="card__text">
+                <span class="title card__item-title">iPhone 11 Pro</span>
+                <span class="price card__item-price">
+                  от
+                  <b>69999 руб.</b>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="card trend__card">
+            <div class="card__img"></div>
+            <div class="card__description">
+              <div class="card__text">
+                <span class="title card__item-title">iPhone 11 Pro</span>
+                <span class="price card__item-price">
+                  от
+                  <b>69999 руб.</b>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="blog content__blog"></div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="content footer__content"></div>
+    </div>
   </div>
 </template>
 
+<script>
+export default {};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+/** Base styles */
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+li,
+a {
+  list-style-type: none;
+  text-decoration: none;
+  color: #ffffff;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+  font-family: Roboto, serif;
+}
+.container {
+  max-width: 1240px;
+  width: 100%;
+  padding: 0 20px;
+  margin: 0 auto;
+  overflow: hidden;
+}
+/** Base styles */
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+/** Components(Elements) styles */
+.main-title {
+  text-align: center;
+  font-size: 32px;
+  font-weight: lighter;
+}
+.cart {
+  position: relative;
+}
+.card {
+  width: 275px;
+  height: 320px;
+  padding: 20px;
+  transition: all 0.3s ease;
+  &:hover {
+    background-color: rgb(241, 241, 241);
+    cursor: pointer;
+  }
+  &__img {
+    margin: 0 auto;
+    background-image: url(./assets/img/card-item.png);
+    width: 240px;
+    height: 200px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+  &__text {
+    height: 80px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    line-height: 25px;
+  }
+}
+.trend {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  &__card {
+    margin: 20px;
+  }
+}
+.icon {
+  display: block;
+  width: 35px;
+  height: 35px;
+}
+.promo {
+  margin-bottom: 20px;
+  &__img {
+    height: 538px;
+    background-image: url(./assets/img/header-promo.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+}
+.navbar {
+  &__icon {
+    fill: white;
+    transition: all 0.3s ease;
+    &:hover {
+      fill: #cf4dbe;
+    }
+  }
+  &__list {
+    display: flex;
+    justify-content: space-between;
+  }
+  &__item {
+    padding-right: 40px;
+  }
+  &__icon {
+    display: block;
+    position: absolute;
+    top: -7px;
+    left: -45px;
+  }
+  &__link {
+    display: inline-block;
+    transition: all 0.3s ease;
+    &:hover {
+      color: #cf4dbe;
     }
   }
 }
+/** Components(Elements) styles */
+
+/** Grid, Conteinters styles */
+.header {
+  color: #fff;
+  background-color: #242424;
+  padding: 20px 0;
+  &__content {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+}
+.content {
+  &__main-title {
+    margin: 30px 0;
+  }
+}
+/** Grid, Conteinters styles */
 </style>

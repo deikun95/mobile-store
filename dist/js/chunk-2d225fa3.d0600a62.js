a96b5be606ae93a203a726fd0c669c6506fa1927
Deikun95 <deikun95@gmail.com>
2020-09-03 23:10:45 +0300

    add dist

---
+++ dist/js/chunk-2d225fa3.d0600a62.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d225fa3"],{e761:function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"wrapper"},[t("h1",[e._v("Новости")])])}],c={},s=c,l=t("2877"),u=Object(l["a"])(s,r,a,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d225fa3.d0600a62.js.map
